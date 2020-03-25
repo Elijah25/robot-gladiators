43e6c2b45c091c21ba16d024becd79931faa127c
Elijah <elijaherap3@gmail.com>
2020-03-25 15:22:45 -0700

    looping fights through multiple enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,17 @@ var playerMoney = 10;
 
 console.log(playerName, playerHealth, playerAttack);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+    // * fight all enemy robots
+    // * defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
+var fight = function(enemyName) {
     window.alert("Welome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -59,4 +65,7 @@ var fight = function() {
 
 }
 
-fight ();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+
