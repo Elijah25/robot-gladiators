ab2db2b03b128fbcbfb667c6819019d67b206bb8
Elijah <elijaherap3@gmail.com>
2020-03-27 19:16:21 -0700

    name prompt bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -61,8 +61,17 @@ var randomNumber = function(min, max){
   return value;
 }
 
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null){
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -107,7 +116,6 @@ var enemyInfo = [
     attack: randomNumber(10, 14)
   }
 ];
-
 
 var startGame = function () {
     playerInfo.reset();
